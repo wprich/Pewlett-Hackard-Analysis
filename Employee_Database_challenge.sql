6dcc9f7ce6f06456a443f1206f38ec06fe8c85ad
William <wprich704@gmail.com>
2021-08-15 14:59:02 -0700

    Part 2 - Mentorship Eligibilty

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,48 @@
+--Create retirement titles table
+select e.first_name, 
+	   e.last_name, 
+	   e.emp_no, 
+	   ti.title,
+	   ti.from_date,
+	   ti.to_date
+into retirement_titles
+from employees as e
+inner join titles as ti
+on e.emp_no = ti.emp_no
+where (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows and create unique titles table
+select distinct on (emp_no) emp_no,
+first_name,
+last_name,
+title
+into unique_titles
+from retirement_titles
+order by emp_no asc, to_date desc;
+
+--Retirement count by job title
+select count(emp_no), title
+into retiring_titles
+from unique_titles
+group by title
+order by count desc
+
+
+--Creating mentorship eligibilty table
+select distinct on(e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+into mentorship_eligibilty
+from employees as e
+left join dept_emp as de
+on e.emp_no = de.emp_no
+left join titles as ti
+on ti.emp_no = e.emp_no
+where de.to_date = ('9999-01-01')
+and (e.birth_date between '1965-01-01' AND '1965-12-31')
+order by e.emp_no asc
